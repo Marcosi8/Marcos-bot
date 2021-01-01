49304f46020aa27e58aa34ea89f7438c82969fb5
Marcosi8 <76185975+Marcosi8@users.noreply.github.com>
2021-01-01 20:45:35 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -113,8 +113,7 @@ _COMANDOS DO DONO DO BOT_
    ╿𝘜𝘵𝘪𝘭𝘪𝘥𝘢𝘥𝘦: Limpa os chats do bot.
    ╿𝘌𝘹𝘦𝘮𝘱𝘭𝘰: Digite ${prefix}clearall`
 
-By: Marcos Oliveira.
-*Fale comigo!* +558897165919
+*Fale comigo!* ➠http://Wa.me/+558897165919
 }
 
 exports.help = help
